89d944052a2a8ba0b33b1eb171f0632fc24e926e
AdityaJainDXB <p7wh8cd5j8@privaterelay.appleid.com>
2026-08-21 14:11:42 +0400

    Add health check server to index.js

--- index.js
+++ index.js
@@ -1103,3 +1103,13 @@ Use \`/slackpilot-tasks\` to view your tasks.`
     console.error(error);
   }
 })();
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end("SlackPilot is running!");
+}).listen(PORT, "0.0.0.0", () => {
+  console.log(`🌐 Health server running on port ${PORT}`);
+});
